f352adbbd691d717eb8ac821262c3639aff90a84
Aditya  Tangirala <Aditya.Tangirala@ggktech.local>
2019-01-21 16:03:54 +0530

    add ace-portal project

---
+++ app.js
@@ -0,0 +1,12 @@
+const express =require('express');
+const app =express();
+const path =require('path');
+app.set('view engine','ejs');
+app.set('views','views');
+app.use(express.static(path.join(__dirname,'public'),{extended:false}));
+app.use('/',(req,res,next)=>{
+    res.render('index');
+})
+app.listen(3000,()=>{
+    console.log("HI welcome to express Js\n");
+});
